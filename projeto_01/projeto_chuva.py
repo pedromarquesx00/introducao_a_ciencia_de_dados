52d7d7e878a2c8780536c31a584da48207905f32
Pedro Marques <pedro.marques@ics.ufal.br>
2026-03-19 15:02:50 -0300

    atualizado

--- projeto_01/projeto_chuva.py
+++ projeto_01/projeto_chuva.py
@@ -68,6 +68,7 @@ chuvas = ["Chuva01","Chuva02","Chuva03","Chuva04",
 # %%
 df_melt = df.melt(id_vars=["Data","NivelConsistencia"],
                   value_vars=chuvas,
+                  ar_name="Chuvas",
                   value_name="QtdeChuva")
 
 # %%
