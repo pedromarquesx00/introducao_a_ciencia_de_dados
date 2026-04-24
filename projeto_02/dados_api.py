9f77cd0588a1c8d4c9c358a39d403ff52dbdb370
pedro-marques <pedro.marques@ics.ufal.br>
2026-04-24 10:50:13 -0300

    atualização projeto 2

---
+++ projeto_02/dados_api.py
@@ -0,0 +1,45 @@
+# %% Bibliotecas usadas
+import requests
+import pandas as pd
+from io import StringIO
+import plotly.express as px
+
+# %% Função para puxar os dados da API
+def estacoesSubBaciaAPI(cod, tpEst):
+    """
+    Busca estações de uma sub-bacia no inventário da ANA (HidroWeb)
+
+    cod: código da sub-bacia (string)
+    tpEst: '1' = fluviométrica  |  '2' = pluviométrica
+    """
+    if (tpEst != '1' and tpEst != '2'):
+      print('ERRO: Informe um valor válido para tpEst. ("1" - fluviométrica; "2" - pluviométrica)')
+    else:
+        url = 'http://telemetriaws1.ana.gov.br/ServiceANA.asmx/HidroInventario'
+        params = {
+            'codEstDE': '',
+            'codEstATE': '',
+            'tpEst': tpEst,
+            'nmEst': '',
+            'nmRio': '',
+            'codSubBacia': cod,
+            'codBacia': '',
+            'nmMunicipio': '',
+            'nmEstado': '',
+            'sgResp': '',
+            'sgOper': '',
+            'telemetrica': ''
+        }
+
+        response = requests.get(url, params = params)
+
+        df = pd.read_xml(StringIO(response.text), xpath = '//Table')
+        return df
+# %%
+df = estacoesSubBaciaAPI("51","1")
+df
+
+# %%
+fig = px.timeline(df,x_start="DataIns",x_end="DataAlt",y="RioNome")
+fig.update_yaxes(autorange="reversed")
+fig.show()
